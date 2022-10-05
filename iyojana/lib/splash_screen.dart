bd137ae95dad2e362eefbb00afdb2070359375be
AkhileshManda <manda.akhilesh4@gmail.com>
2022-10-05 13:45:07 +0530

    bug fix : made login screen scrollable

--- iyojana/lib/splash_screen.dart
+++ iyojana/lib/splash_screen.dart
@@ -6,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
